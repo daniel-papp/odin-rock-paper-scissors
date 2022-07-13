f508f6e853394fb3fc8b0f23b3139f527219759b
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-07-13 20:29:26 +0200

    Declare the playRound function

--- script.js
+++ script.js
@@ -21,3 +21,53 @@ function computerPlay() {
     return computerSelection;
 }
 
+// playRound: compares the player's and the computer's choice and declares the winner
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    let winner;
+    
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'rock') {
+            winner = 'Tie!';
+        } else if (computerSelection === 'paper') {
+            winner = 'You Lose! Paper beats Rock';
+        } else if (computerSelection === 'scissors') {
+            winner = 'You Win! Rock beats Scissors';
+        } else {
+            winner = 'Somebody cheated!'
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            winner = 'You Win! Paper beats Rock';
+        } else if (computerSelection === 'paper') {
+            winner = 'Tie!';
+        } else if (computerSelection === 'scissors') {
+            winner = 'You Lose! Scissors beats Paper';
+        } else {
+            winner = 'Somebody cheated!'
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            winner = 'You Lose! Rock beats Scissors';
+        } else if (computerSelection === 'paper') {
+            winner = 'You Win! Scissors beats Paper';
+        } else if (computerSelection === 'scissors') {
+            winner = 'Tie!';
+        } else {
+            winner = 'Somebody cheated!'
+        }
+    } else {
+        winner = 'Somebody cheated!'
+    }
+
+    return winner;
+
+}
+
+// Testing the playRound function
+// const playerSelection = 'rOck';
+// const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));
