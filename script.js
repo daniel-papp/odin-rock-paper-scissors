699da595eadadc186ced09907b5733ba05d80ba6
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-07-13 19:29:52 +0200

    Declare the computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Hello Game");
+
+// ----- Function Declarations -----
+
+// computerPlay: randomly generates a choice for the computer
+
+function computerPlay() {
+    const computerSelectionNumeric = Math.floor(Math.random() * 3);
+    let computerSelection;
+    switch(computerSelectionNumeric) {
+        case 0 :
+            computerSelection = 'rock';
+            break;
+        case 1 :
+            computerSelection = 'paper';
+            break;
+        case 2 :
+            computerSelection = 'scissors';
+            break;
+    }
+    return computerSelection;
+}
+
