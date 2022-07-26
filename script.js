7fe518a6ab03d4b2078f16a63a7e287785c46598
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-07-26 20:03:28 +0200

    Clean up the JavaScript code

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-    console.log("Let's play a game of Rock Paper Scissors!");
-
 // ----- Function Declarations -----
 
 // computerPlay: randomly generates a choice for the computer
@@ -7,6 +5,7 @@
 function computerPlay() {
     const computerSelectionNumeric = Math.floor(Math.random() * 3);
     let computerSelection;
+    
     switch(computerSelectionNumeric) {
         case 0 :
             computerSelection = 'rock';
@@ -24,8 +23,6 @@ function computerPlay() {
 // playRound: compares the player's and the computer's choice and declares the winner
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
 
     let winner;
     
@@ -53,49 +50,72 @@ function playRound(playerSelection, computerSelection) {
         } else if (computerSelection === 'scissors') {
             winner = 'Tie!';
         }
-    } else {
-        winner = 'Invalid choice, try again.'
     }
 
-// Add display for round results
-
-
     return winner;
-
 }
 
-// game: plays 5 rounds and records the score
+// keepScore: calls the playRound function and updates the score based on the result
+// it also outputs the current round, result, and score to the DOM
 
-// function game() {
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     for (let i = 0; i < 5; i++) {
-//         let playerSelection = prompt('Make your choice!');
-//         let computerSelection = computerPlay();
-//         let winnerMessage = playRound(playerSelection, computerSelection);
-//         if (winnerMessage.includes('Win!')) {
-//             playerScore++;
-//         } else if (winnerMessage.includes('Lose!')) {
-//             computerScore++;
-//         } else if (winnerMessage.includes('Invalid')) {
-//             i--;
-//         }
-//         console.log(`Round ${i + 1}`);
-//         console.log(winnerMessage);
-//         console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
-//     }
-//     (playerScore > computerScore) ? console.log('Congratulations, you won the game!') : console.log('Unfortunately, you lost the game.');
-//     console.log(`Final score: Player: ${playerScore} - Computer: ${computerScore}`);
-// }
+function keepScore(playerSelection) {
+    currentRound++;
+    let winnerMessage = playRound(playerSelection, computerPlay());
 
-// Add a button to start the game
+    if (winnerMessage.includes('Win!')) {
+        playerScore++;
+    } else if (winnerMessage.includes('Lose!')) {
+        computerScore++;
+    }
+             
+    // Display info on the DOM
 
-// const startButton = document.querySelector('#start-game');
-// startButton.addEventListener('click', game);
+    const display = document.querySelector('#display');
 
+    const roundCountText = document.createElement('p');
+    roundCountText.textContent = `Round ${currentRound}`;
+    display.appendChild(roundCountText);
 
+    const roundResultText = document.createElement('p');
+    roundResultText.textContent = winnerMessage;
+    display.appendChild(roundResultText);
 
-// Add 3 buttons for player selection
+    const roundScoreText = document.createElement('p');
+    roundScoreText.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
+    display.appendChild(roundScoreText);    
+}
+
+// playGame: calls the keepScore function and displays the final result of the game after 5 rounds
+    
+function playGame(playerSelection) {
+    keepScore(playerSelection);
+
+    if (currentRound === 5) {
+        const display = document.querySelector('#display');
+        const finalResultText = document.createElement('p');
+        const finalScoreText = document.createElement('p');
+
+        if (playerScore > computerScore) {
+            finalResultText.textContent = 'Congratulations, you won the game!';
+        } else if (playerScore < computerScore) {
+            finalResultText.textContent = 'Unfortunately, you lost the game.';
+        } else {
+            finalResultText.textContent = 'It\'s a tie!';
+        }
+
+        display.appendChild(finalResultText);
+        finalScoreText.textContent = `Final score: Player: ${playerScore} - Computer: ${computerScore}`;
+        display.appendChild(finalScoreText);
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let currentRound = 0;
+
+// ----- DOM Events -----
+
+// Add 3 buttons for player selection an
 
 const rockButton = document.querySelector('#rock-btn');
 rockButton.addEventListener('click', () => playGame('rock'));
@@ -105,66 +125,3 @@ paperButton.addEventListener('click', () => playGame('paper'));
 
 const scissorsButton = document.querySelector('#scissors-btn');
 scissorsButton.addEventListener('click', () => playGame('scissors'));
-
-
-// Add event listener to buttons for future scorekeeping
-
-// const selectButtons = document.querySelectorAll('.button');
-// selectButtons.forEach(button => button.addEventListener('click', keepScore));
-
-
-
-// Function for keeping score
-
-// console.log(playRound(playerSelection, computerPlay()));
-function keepScore(playerSelection) {
-    currentRound++;
-    let winnerMessage = playRound(playerSelection, computerPlay());
-    if (winnerMessage.includes('Win!')) {
-        playerScore++;
-    } else if (winnerMessage.includes('Lose!')) {
-            computerScore++;
-        }
-             
-        // Display info on the DOM
-        const display = document.querySelector('#display');
-        const roundCountText = document.createElement('p');
-        roundCountText.textContent = `Round ${currentRound}`;
-        display.appendChild(roundCountText);
-        const roundResultText = document.createElement('p');
-        roundResultText.textContent = winnerMessage;
-        display.appendChild(roundResultText);
-        const roundScoretText = document.createElement('p');
-        roundScoretText.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
-        display.appendChild(roundScoretText);    
-    }
-    
-    
-    // Function for playing an entire game (final score after 5 rounds)
-    
-let playerScore = 0;
-let computerScore = 0;
-let currentRound = 0;
-function playGame(playerSelection) {
-    keepScore(playerSelection);
-    if (currentRound === 5) {
-        const display = document.querySelector('#display');
-        const finalResultText = document.createElement('p');
-        const finalScoreText = document.createElement('p');
-        if (playerScore > computerScore) {
-            finalResultText.textContent = 'Congratulations, you won the game!';
-        } else if (playerScore < computerScore) {
-            finalResultText.textContent = 'Unfortunately, you lost the game.';
-        } else {
-            finalResultText.textContent = 'It\'s a tie!';
-        }
-        display.appendChild(finalResultText);
-        finalScoreText.textContent = `Final score: Player: ${playerScore} - Computer: ${computerScore}`;
-        display.appendChild(finalScoreText);
-    }
-}
-
-
-
-
-// commit message: Add round count and final results
